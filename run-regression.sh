b1e631a690a30a90f4d77481c913720b32323ae7
Nigel Babu <nigelb@redhat.com>
2017-01-18 12:07:20 +0530

    Add the gluster-api-devel package as well

--- run-regression.sh
+++ run-regression.sh
@@ -9,5 +9,5 @@ EOF
 mknod /dev/loop0 b 7 0
 mknod /dev/loop1 b 7 0
 mknod /dev/loop2 b 7 0
-dnf install -y --nogpgcheck glusterfs-server glusterfs-fuse glusterfs-api glusterfs-cli glusterfs-debuginfo glusterfs-devel glusterfs-libs glusterfs-regression-tests
+dnf install -y --nogpgcheck glusterfs-server glusterfs-fuse glusterfs-api glusterfs-api-devel glusterfs-cli glusterfs-debuginfo glusterfs-devel glusterfs-libs glusterfs-regression-tests
 /opt/qa/regression.sh -- tests/basic
