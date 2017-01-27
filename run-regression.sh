e26ca737b9cad23f06902be5ee8dcfa447928983
Nigel Babu <nigelb@redhat.com>
2017-01-27 12:25:22 +0530

    Use notes from rtalur's scripts for loop

--- run-regression.sh
+++ run-regression.sh
@@ -1,14 +1,31 @@
+#!/bin/bash
+set -e
 cd /code
 mkdir -p /build /var/log/glusterfs
+mkdir -p /run/rpcbind/
 cat <<EOF > /etc/yum.repos.d/gluster-build.repo
 [gluster-build]
 name = Gluster Build
 baseurl = file:///opt/qa/rpm
 EOF
+# Run rpcbind for gNFS
 /usr/sbin/rpcbind -w
-mknod /dev/loop0 b 7 0
-mknod /dev/loop1 b 7 0
-mknod /dev/loop2 b 7 0
+
+mknod /dev/loop0 -m0660 b 7 0 || true
+mknod /dev/loop1 -m0660 b 7 1 || true
+mknod /dev/loop2 -m0660 b 7 2 || true
+mknod /dev/loop3 -m0660 b 7 3 || true
+mknod /dev/loop4 -m0660 b 7 4 || true
+mknod /dev/loop5 -m0660 b 7 5 || true
+mknod /dev/loop6 -m0660 b 7 6 || true
+mknod /dev/loop7 -m0660 b 7 7 || true
+mknod /dev/loop8 -m0660 b 7 8 || true
+mknod /dev/loop9 -m0660 b 7 9 || true
+mknod /dev/loop10 -m0660 b 7 10 || true
+mknod /dev/loop11 -m0660 b 7 11 || true
+mknod /dev/loop12 -m0660 b 7 12 || true
+mknod /dev/loop13 -m0660 b 7 13 || true
+
 dnf install -y --nogpgcheck glusterfs-server glusterfs-fuse glusterfs-api glusterfs-api-devel glusterfs-cli glusterfs-debuginfo glusterfs-devel glusterfs-libs glusterfs-regression-tests
 if [ -z "$1" ]
 then
